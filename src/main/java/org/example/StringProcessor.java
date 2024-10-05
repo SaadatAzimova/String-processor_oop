300b2bb17d24f2e8628d20bfb8178f5b19e17b01
SaadatAzimova <saadatazimova5@gmail.com>
2024-10-05 15:51:18 +0600

    Calculate words

--- src/main/java/org/example/StringProcessor.java
+++ src/main/java/org/example/StringProcessor.java
@@ -2,15 +2,16 @@ package org.example;
 
 public class StringProcessor {
     public  boolean isStrongPassword(String password) {
-        //1.check length of the password
+        //checking length of the password
         if (password.length() < 8) {
             return false;
         }
+        // setting boolen to false
         boolean hasUppercase = false;
         boolean hasLowercase = false;
         boolean hasDigit = false;
         boolean hasSpecialChar = false;
-
+        // then if the password has upper case, lower case,digit, spcial number changing boolean to true
         for (int i = 0; i < password.length(); i++) {
             char ch = password.charAt(i);
             if (Character.isUpperCase(ch)) {
@@ -26,19 +27,33 @@ public class StringProcessor {
                 hasSpecialChar = true;
             }
         }
-
+        //return if all 4 conditions true
         return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
     }
     public int calculateDigits(String sentence) {
+        //initially setting digit count to 0
         int digitCount = 0;
 
         //check each character in the sentence
         for (int i = 0; i < sentence.length(); i++) {
             //check if the character is a digit
             if (Character.isDigit(sentence.charAt(i))) {
-                digitCount++;
+                digitCount++;// adding 1 to digitCout if we have digits in the sentence
             }
         }
         return digitCount;
+    }
+    public int calculateWords(String sentence) {
+        //case when we dont have any words in the sentence
+        if (sentence == null || sentence.length() == 0) {
+            return 0;
+        }
+        //case with words
+                int wordCount = 1;//because there is no white space when we have only 1 word in the sentence
+                for (int i = 0; i < sentence.length(); i++) {//loop goes through evere character looking for the white space
+                    if (Character.isWhitespace(sentence.charAt(i))) {
+                        wordCount++; //if there is white space we assume that there are more than 1 word and add to wordCount
+                    }
+                }return wordCount;
     }
 }
--- src/test/java/StringProcessorTest.java
+++ src/test/java/StringProcessorTest.java
@@ -7,15 +7,29 @@ public class StringProcessorTest {
     @Test
     public void isStrongPasswordTest() {
         StringProcessor password = new StringProcessor();
-        assertTrue(password.isStrongPassword("Qwerty12345*"));
-        assertFalse(password.isStrongPassword("qwerty12345*"));
-
-
+        assertTrue(password.isStrongPassword("Qwerty12345*"));//true password
+        assertTrue(password.isStrongPassword("OopCourse2024!"));//true password
+        assertFalse(password.isStrongPassword("qwerty12345*"));//false,password without Upper case letter
+        assertFalse(password.isStrongPassword("oopCourse"));//false,password without digits
+        assertFalse(password.isStrongPassword(""));//false,password without characters
     }
     @Test
     public void calculateDigitsTest() {
         StringProcessor sentence = new StringProcessor();
-        assertEquals(2, sentence.calculateDigits("fyu2fuyuy3"));
-        assertNotEquals(2, sentence.calculateDigits("fyu2fuyuy3"));
+        assertEquals(2, sentence.calculateDigits("oop1semester1"));//Equal, with digit
+        assertEquals(0, sentence.calculateDigits("oop"));//equal, without any digit
+        assertNotEquals(0,sentence.calculateDigits("oop1semester1"));//not equal, with digit
+        assertNotEquals(3, sentence.calculateDigits("oop"));//not equal,without  any digit
+        assertNotEquals(1,"");//not equal, witout any charaacter
     }
+    @Test
+    public void calculateWordsTest() {
+        StringProcessor sentence = new StringProcessor();
+        assertEquals(1, sentence.calculateWords("oop"));//equal, 1 word(1 word)
+        assertEquals(2, sentence.calculateWords("oop course"));//equal,2 words(with white space)
+        assertEquals(0, sentence.calculateWords(""));//equal, testing null case
+        assertNotEquals(3, sentence.calculateWords("oop course"));//not equal
+        assertNotEquals(2, sentence.calculateWords(""));//not equal, null case
+    }
+
 }
